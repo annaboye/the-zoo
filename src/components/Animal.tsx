7c77882ada9f10fa2d9496b31401e8761261bf15
annaboye <boye.anna@gmail.com>
2023-05-03 11:44:47 +0200

    (feat): change isFed, display info

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 
 
 export const Animal = ({id, imageUrl, isFed, lastFed, latinName, longDescription, medicine, name, shortDescription, fullDesc, animalList}: IAnimalProps) =>{
-  const [fed, setFed] = useState(lastFed)
+  const [fedTime, setFedTime] = useState(lastFed)
+  const [fed, setfed]= useState(isFed)
    const addDefaultSrc =(e: SyntheticEvent)=>{
      let imgTag= e.target as HTMLImageElement;
      imgTag.src ="https://gotogulf.com/images/person-default.png"
@@ -15,31 +16,35 @@ export const Animal = ({id, imageUrl, isFed, lastFed, latinName, longDescription
     const currentAnimal = animalList.find((animal)=> animal.id === id)
     
     if(currentAnimal){currentAnimal.lastFed = new Date().toString();
-    setFed(currentAnimal.lastFed)}
+      currentAnimal.isFed =true;
+    setFedTime(currentAnimal.lastFed)
+  setfed(currentAnimal.isFed)}
+   
     localStorage.setItem("animals",JSON.stringify(animalList))
     }
 
    if (fullDesc===false){
    return (<>
    <div className="forall"><h3>{name}</h3>
+   {!fed && <h2 className="hungry">JAG ÄR HUNGRIG!</h2>}
+   {fed && <h2 className="full">JAG ÄR MÄTT!</h2>}
    <Link to={JSON.stringify(id)}><img src={imageUrl} onError={(event)=>addDefaultSrc(event)}alt={latinName} /> </Link>
-   <p>Senast matad: {fed}</p>
+   <p>Senast matad: {fedTime}</p>
    <p>{shortDescription}</p>
-   <button className={isFed?"full":"needfood"} onClick={()=>fedTheAnimal(id)}>mata</button>
    </div>
   </>)}
   else{
     return (<>
     <div className="forone">
     <h3>{name}</h3>
+    {!fed && <h2 className="hungry">JAG ÄR HUNGRIG!</h2>}
+    {fed && <h2 className="full">JAG ÄR MÄTT!</h2>}
     <img src={imageUrl} onError={(event)=>addDefaultSrc(event)}alt={latinName} />
     <p>{longDescription}</p>
-    <p>Senast matad: {fed}</p>
+    <p>Senast matad: {fedTime}</p>
     <p>latinskt namn: {latinName}</p>
     <p>Mediciner:{medicine}</p>
-    
-   
-    <button className={isFed?"full":"needfood"} onClick={()=>fedTheAnimal(id)}>mata</button>
+    <button className={fed?"full":"needfood"} onClick={()=>fedTheAnimal(id)}>mata</button>
     </div>
    </>)}
   
